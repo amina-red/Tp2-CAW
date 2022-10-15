625d44227849862064ef87014fa47089a415b97c
amina-red <115896743+amina-red@users.noreply.github.com>
2022-10-15 21:09:50 +0100

    Create exo6.js

---
+++ exo6.js
@@ -0,0 +1,39 @@
+//EXO6
+var lose = false; 
+var x =document.getElementsByClassName("boundary example")
+console.log(x);
+window.onload = function() {
+    document.getElementById("start").onclick = startGame;
+
+    document.getElementById("end").onmouseover = endGame;
+
+    div_shape = document.getElementsByClassName("boundary");
+
+    for (var i = 0; i < div_shape.length-1; i++) {
+        div_shape[i].onmouseover = mouse_over;
+    }
+};
+function mouse_over() {
+    lose = true;
+    div_shape = document.getElementsByClassName("boundary");
+
+    for (var i = 0; i < div_shape.length-1; i++) {
+        div_shape[i].style.backgroundColor = "#ff8888";
+    }
+}
+function startGame() {
+    lose = false;
+    div_shape = document.getElementsByClassName("boundary");
+
+    for (var i = 0; i < div_shape.length-1; i++) {
+        div_shape[i].style.backgroundColor = "#eeeeee";
+    }
+}
+function endGame() {
+    if(lose) {
+         x[0].innerHTML="lose";
+           
+    } else {
+        x[0].innerHTML="win";
+    }
+}
