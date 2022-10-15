73dbbf6ccd51929fb041c4ffb9d923532f2665bb
amina-red <115896743+amina-red@users.noreply.github.com>
2022-10-15 21:09:03 +0100

    Create exo4.js

---
+++ exo4.js
@@ -0,0 +1,36 @@
+//exo4
+var lose = false; 
+window.onload = function() {
+    document.getElementById("start").onmouseover = startGame;
+
+    document.getElementById("end").onmouseover = endGame;
+
+    div_shape = document.getElementsByClassName("boundary");
+
+    for (var i = 0; i < div_shape.length-1; i++) {
+        div_shape[i].onmouseover = mouse_over;
+    }
+};
+function mouse_over() {
+    lose = true;
+    div_shape = document.getElementsByClassName("boundary");
+
+    for (var i = 0; i < div_shape.length-1; i++) {
+        div_shape[i].style.backgroundColor = "#ff8888";
+    }
+}
+function startGame() {
+    lose = false;
+    div_shape = document.getElementsByClassName("boundary");
+
+    for (var i = 0; i < div_shape.length-1; i++) {
+        div_shape[i].style.backgroundColor = "#eeeeee";
+    }
+}
+function endGame() {
+    if(lose) {
+        alert("you lose!");
+    } else {
+        alert("You win!");
+    }
+}
