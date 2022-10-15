79e73787106071a4a7801d4c1ce072299e80b69c
amina-red <115896743+amina-red@users.noreply.github.com>
2022-10-15 21:03:12 +0100

    Create exo2.js

---
+++ exo2.js
@@ -0,0 +1,16 @@
+// exo2 
+  window.onload = function() {
+    div_shape = document.getElementsByClassName("boundary");
+
+    for (var i = 0; i < div_shape.length-1; i++) {
+        div_shape[i].onmouseover = mouse_over;
+    }
+};
+
+function mouse_over() {
+    div_shape = document.getElementsByClassName("boundary");
+
+    for (var i = 0; i < div_shape.length-1; i++) {
+        div_shape[i].style.backgroundColor = "#ff8888";
+    }
+}
