0f91c4d71972154f0e00bc6e484eaca0e04939aa
amina-red <115896743+amina-red@users.noreply.github.com>
2022-10-15 21:07:14 +0100

    Create exo3.js

---
+++ exo3.js
@@ -0,0 +1,24 @@
+//exo3
+window.onload = function() {
+    document.getElementById("end").onmouseover = endGame;
+    div_shape = document.getElementsByClassName("boundary");
+
+    for (var i = 0; i < div_shape.length-1; i++) {
+        div_shape[i].onmouseover = mouse_over;
+    }
+};
+
+function mouse_over() {
+
+    div_shape = document.getElementsByClassName("boundary");
+
+    for (var i = 0; i < div_shape.length-1; i++) {
+        div_shape[i].style.backgroundColor = "#ff8888";
+    }
+}
+
+function endGame() {
+ 
+        alert("You win!");
+   
+}
